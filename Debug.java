9d1d2f509c7c4f80d82cc5d23e2413fdb2dcd291
gavboi <50150638+gavboi@users.noreply.github.com>
2019-05-01 13:11:48 -0400

    Update Debug.java
    added method support for multiple classes

--- Debug.java
+++ Debug.java
@@ -1,14 +1,17 @@
 public class Debug {
 
 	public static void main(String[] args) {
-		
-    //DUNGEON.JAVA
+
+		dungeon();
+
+	}
+
+	public static void dungeon() {
 		Dungeon d = new Dungeon(8, 0.9, 0.4, false);
-		
+
 		d.newRoom();
 		System.out.print(d.room);
 		d.printGeneralInfo();
-
 	}
 
 }
