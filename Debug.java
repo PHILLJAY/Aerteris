d7fd8888c54b3afca17c9ba689eaece9331aad2c
gavboi <50150638+gavboi@users.noreply.github.com>
2019-05-06 14:19:54 -0400

    Update Debug.java
    - battles

--- Debug.java
+++ Debug.java
@@ -5,31 +5,38 @@ public class Debug {
 		//inventory();
 		dungeon();
 		//doWall();
+		//fighting();
 
 	}
 	
 	public static void inventory() {
 		Inv test =  new Inv();
 		test.place(0, 1);
-		test.place(1, 3);
+		test.place(1, 2);
 		test.showInv();
-//		test.place(0, 2);
-//		test.showInv();
-		System.out.println(test.printDesc(4));
+		test.place(0, 2);
+		test.showInv();
 		
 	}
 
 	public static void dungeon() {
 		int s = 8;
+		Charac player = new Charac(20,3,0.2,0);
 
 		Dungeon d = new Dungeon(s, 0.7, 0.4, 0.1, 0.25, 0.25); //decent setup for basic dungeon according to individual tests: n, 0.7, 0.4, 0.1, 0.25, 0.25
 		
-		d.enterDungeon();
+		d.enterDungeon(player);
 	}
 
 	public static void doWall() { //NOT DONE THIS WAY IN THE DUNGEON CLASS!! THIS IS ONLY A TEST
 			double r = Math.random();
 			System.out.print(r + ", " + (r * (1 - 1 * 0.2)) + ", " + (r * (1 - 2 * 0.2)));
+	}
+	
+	public static void fighting() {
+		Charac player = new Charac(20,3,0.2,0);
+		Monster monster = new Monster((int)(10+Math.floor(Math.random()*7)-3), (1), (0.1), (0), "TESTSUBJECTSKELE");
+		Battle b = new Battle(player, monster);
 	}
 	
 }
