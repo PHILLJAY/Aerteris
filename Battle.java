a2ddb98c891ceb822c695444cd794cdf9af88abd
Dowxu <47536134+Dowxu@users.noreply.github.com>
2019-05-06 19:43:59 -0400

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -3,82 +3,74 @@ import java.util.*;
 
 public class Battle {
 
-	 public static Scanner scan = new Scanner(System.in);
+	public static Scanner scan = new Scanner(System.in);
 	private char n;
 
 	public Battle(charac x,Monster y ) {
+		System.out.println("\n-------------------------------");
+		System.out.println("# "+y.name+" Appears! # \n");
+		
 		while(true) {
 			if(isDead(x)==false) {
+				System.out.println("Your HP: "+x.currenthealth);
+				System.out.println(y.name+ "'s HP: "+y.currenthealth +"\n");
 				if((n=move())=='a') {
 					attack(x,y);
-					System.out.println("deals "+ x.attack+" damage "+
-					y.name+ " HP: "+y.currenthealth );
-				} else if(n=='r') break;
+					System.out.println("> You deal "+ (x.attack-y.defense)+" damage to "+y.name);
+				} else if(n=='r') { System.out.println("You ran away!\n"); break;}
 			} else {
-				System.out.print("you died");
+				System.out.print("You died");
 				break;
 			}
-			
+
 			if(isMonsterDead(y)==false) {
-			monsterattack(x,y);
-			System.out.println(y.name +" deals " + y.attack +" damage"+
-					" player HP: "+ x.currenthealth);
-			
+				monsterattack(x,y,"a");
+				System.out.println("> "+y.name +" deals " + (y.attack-x.defense) +" damage \n");
 			}else {
 				System.out.print("you won");
 				break;
 			}
 		}
 	}
-	
-	
-	public static void attack(charac x, Monster y){
-		
-		y.currenthealth=y.currenthealth+y.defense-x.attack;
+
+
+	public static void attack(charac x, Monster y){	
+		if(Math.random()<=x.crit) {
+			y.currenthealth=y.currenthealth+y.defense-x.attack*2;
+		}else {	y.currenthealth=y.currenthealth+y.defense-x.attack;
+		}
 	}
-	
-	public static void monsterattack(charac y, Monster x){
-		
-		y.currenthealth=y.currenthealth+y.defense-x.attack;
+
+	public static void monsterattack(charac p, Monster m,String s){
+		if(s.equals("a")) {
+			if(Math.random()<=m.crit) {
+				p.currenthealth+=p.defense-m.attack*2;
+			}else {	p.currenthealth+=p.defense-m.attack;
+			}
+		} else if(s.equals("sp")) {
+			p.currenthealth+=p.defense-m.attack*3;
+		}
 	}
-	
-	
-	
-	
+
+
+
 	public char move() {
-		System.out.print("whatchu wanna do: attack, run, item, ");
+		System.out.print("Whatchu wanna do: \nAttack \nRun \nItem \n");
 		char c = scan.nextLine().charAt(0);
 		return c;		
 	}
-	
+
 	public static boolean isDead(charac x) {
-		if(x.currenthealth<=0) {
+		if(x.currenthealth<1) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public static boolean isMonsterDead(Monster x) {
-		if(x.currenthealth<=0) {
+		if(x.currenthealth<1) {
 			return true;
 		}
 		return false;
 	}
-	
-	public int GetHealth(charac x) {
-		return x.currenthealth;
-	}
-	
-	public int GetDefense(charac x) {
-		return x.defense;
-	}
-	public int GetAttack(charac x) {
-		return x.attack;
-	}
-	public int Getmaxhealth(charac x) {
-		return x.maxhealth;
-	}
-	
-	
-	
 }
