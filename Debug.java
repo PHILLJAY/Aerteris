3c8af3e0ae3ccb9484716067f03a886ff982b9c0
gavboi <50150638+gavboi@users.noreply.github.com>
2019-05-08 13:48:06 -0400

    Update Debug.java

--- Debug.java
+++ Debug.java
@@ -5,9 +5,9 @@ public class Debug {
 	public static void main(String[] args) {
 
 		//inventory();
-		//dungeon();
+		dungeon();
 		//doWall();
-		fighting();
+		//fighting();
 
 	}
 	
@@ -23,7 +23,7 @@ public class Debug {
 
 	public static void dungeon() {
 		int s = 8;
-		Charac player = new Charac(20,3,0.2,0);
+		charac player = new charac(20,3,0.2,0);
 
 		Dungeon d = new Dungeon(s, 0.7, 0.4, 0.1, 0.25, 0.25); //decent setup for basic dungeon according to individual tests: n, 0.7, 0.4, 0.1, 0.25, 0.25
 		
@@ -36,7 +36,7 @@ public class Debug {
 	}
 	
 	public static void fighting() {
-		Charac player = new Charac(40,7,0.2,0);
+		charac player = new charac(40,7,0.2,0);
 		Monster monster = new Monster(
 				(int)((player.maxhealth/2)+Math.floor(Math.random()*(player.maxhealth/2))-(player.maxhealth/4)), 
 				(int)((player.attack/2)+Math.floor(Math.random()*(player.attack/2))-(player.attack/4)), 
